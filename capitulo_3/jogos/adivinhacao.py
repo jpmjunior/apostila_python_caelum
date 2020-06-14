a682bfc0aa61bc7d579f3ca6aaf928dc198faf93
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-14 17:37:27 -0300

    Exercício 3.12.2 - Incluído o cabeçalho do jogo

---
+++ capitulo_3/jogos/adivinhacao.py
@@ -0,0 +1,8 @@
+# Arquivo: programa.py (UTF-8)
+# Descrição: Jogo da adivinhação
+# Autor: Pinheiro Jr.
+# Data: 14/06/2020
+
+print('******************************')
+print('*    Jogo da adivinhação     *')
+print('******************************')
