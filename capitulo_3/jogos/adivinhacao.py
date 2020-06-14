36da6691be2780fa01a5b2ab0edfea29732bb31b
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-14 20:55:17 -0300

    Exercício 3.16.4 - Adicionado 3 níveis de dificuldade

--- capitulo_3/jogos/adivinhacao.py
+++ capitulo_3/jogos/adivinhacao.py
@@ -5,11 +5,18 @@
 
 print('\n******************************')
 print('*    Jogo da adivinhação     *')
-print('******************************')
+print('******************************\n')
 
 numero_secreto = 42
-total_de_tentativas = 3
 rodada = 1
+nivel = input('Escolha um nível (1 = fácil, 2 = médio, 3 = difícil): ')
+
+if nivel == '1':
+    total_de_tentativas = 20
+elif nivel == '2':
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print('\nTentativa {} de {}'.format(rodada, total_de_tentativas))
