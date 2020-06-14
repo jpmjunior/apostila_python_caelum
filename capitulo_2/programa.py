deb682fd3e041cb1d223a80f95d148603811cebb
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-14 17:17:12 -0300

    Exercício 2.12.2 - Imprimindo duas linhas

--- capitulo_2/programa.py
+++ capitulo_2/programa.py
@@ -1,6 +1,7 @@
 # Arquivo: programa.py (UTF-8)
-# Função: Escreve uma linha na console
+# Função: Escreve duas linhas na console
 # Autor: Pinheiro Jr.
 # Data: 14/06/2020
 
 print('Olá José, seja bem vindo ao Pyhton 3!')
+print('Programa escrito em 14/06/2020.')
