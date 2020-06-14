fe93ea06c9c0e554579e259694c89877fac4c463
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-14 17:43:42 -0300

    Exercício 3.12.5 - Comparando numero_secreto com chute usando if

--- capitulo_3/jogos/adivinhacao.py
+++ capitulo_3/jogos/adivinhacao.py
@@ -11,3 +11,8 @@ numero_secreto = 42
 
 chute = input('Digite o seu número: ')
 print('Você digitou: ', chute)
+
+if(numero_secreto == chute):
+    print('Você acertou!')
+else:
+    print('Você errou!')
