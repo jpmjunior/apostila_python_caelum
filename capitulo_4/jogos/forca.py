679d473c287c2bb835112a0f7d1dddfe9b9d87f6
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-15 17:21:54 -0300

    Exercício 4.1.2 - Mensagem de abertura

---
+++ capitulo_4/jogos/forca.py
@@ -0,0 +1,8 @@
+# Arquivo: forca.py (UTF-8)
+# Descrição: Jogo da forca
+# Autor: Pinheiro Jr.
+# Data: 15/06/2020
+
+print('\n*********************************')
+print('***Bem vindo ao jogo da Forca!***')
+print('*********************************\n')
