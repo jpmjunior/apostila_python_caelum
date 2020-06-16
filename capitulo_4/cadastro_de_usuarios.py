8741ff1958038dad9821491c0e3b954956e6a32d
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-16 15:08:20 -0300

    Exercício 4.5.4 - Leitura de dados

--- capitulo_4/cadastro_de_usuarios.py
+++ capitulo_4/cadastro_de_usuarios.py
@@ -2,3 +2,9 @@
 # Descrição: Programa para cadastro de usuários usando dict_usuario. Exercício 4.5.4
 # Autor: Pinheiro Jr.
 # Data: 16/06/2020
+
+dict = {}
+
+dict['nome'] = input('Informe o nome: ')
+dict['idade'] = input('Informe a idade: ')
+dict['cidade'] = input('Informe a cidade: ')
