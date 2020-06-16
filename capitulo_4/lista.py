832f42dbfeb91e5cb4e1ec15a4ada12a87939ec5
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-16 10:02:56 -0300

    Exercício 4.5.1 - c) Impressão de números pares

--- capitulo_4/lista.py
+++ capitulo_4/lista.py
@@ -7,3 +7,8 @@ lista = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8, 3, 3, -52]
 
 print('Maior elemento: ', max(lista))
 print('Menor elemento: ', min(lista))
+
+print('Números pares: ', end='')
+for numero in lista:
+    if numero % 2 == 0:
+        print('{}; '.format(numero), end='')
