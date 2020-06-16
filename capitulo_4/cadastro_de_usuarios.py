be7ed0daa2887ad79269118f696984b35ff861c3
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-16 15:30:23 -0300

    Exercício 4.5.5 - Incluído bloco while

--- capitulo_4/cadastro_de_usuarios.py
+++ capitulo_4/cadastro_de_usuarios.py
@@ -1,15 +1,21 @@
 # Arquivo: cadastro_de_usuarios.py (UTF-8)
-# Descrição: Programa para cadastro de usuários usando dict_usuario. Exercício 4.5.4
+# Descrição: Programa para cadastro de usuários usando dict_usuario. Exercício 4.5.5
 # Autor: Pinheiro Jr.
 # Data: 16/06/2020
 
 dict_usuario = {}
+lista_usuarios = []
+resposta = 'S'
 
-dict_usuario['nome'] = input('\nInforme o nome: ')
-dict_usuario['idade'] = input('Informe a idade: ')
-dict_usuario['cidade'] = input('Informe a cidade: ')
+while resposta.upper() == 'S':
+    dict_usuario['nome'] = input('\nInforme o nome: ')
+    dict_usuario['idade'] = input('Informe a idade: ')
+    dict_usuario['cidade'] = input('Informe a cidade: ')
 
-print()
+    lista_usuarios.append(dict_usuario)
+
+    resposta = input('\nDeseja cadastrar outro usuário? (s/n)\n')
+
 for key in dict_usuario.keys():
     print(key + ': ' + dict_usuario[key])
 print()
