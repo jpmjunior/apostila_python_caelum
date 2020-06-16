8808c2b554f21abe0ea1cf42357a108997caf143
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-16 11:43:44 -0300

    Exercício 4.5.2 - Leitura e impressão de dados

--- capitulo_4/cadastro_de_usuario.py
+++ capitulo_4/cadastro_de_usuario.py
@@ -2,3 +2,15 @@
 # Descrição: Programa Cadastro de Usuário. Exercício 4.5.2
 # Autor: Pinheiro Jr.
 # Data: 16/06/2020
+
+lista = []
+
+nome = input('\nInforme o nome: ')
+sobrenome = input('Informe o sobrenome: ')
+idade = input('Informe a idade: ')
+
+lista.append(nome)
+lista.append(sobrenome)
+lista.append(idade)
+
+print('\nDados cadastrados: {}\n'.format(lista))
