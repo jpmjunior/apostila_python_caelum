8c8edc0ebb02da8b98724db23d19a56cd16d5488
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-16 10:21:00 -0300

    Exercício 4.5.1 - Inclusão de comentários

--- capitulo_4/lista.py
+++ capitulo_4/lista.py
@@ -5,13 +5,16 @@
 
 lista = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8, 3, 3, -52]
 
+# Imprime maior e menor elemento
 print('Maior elemento: ', max(lista))
 print('Menor elemento: ', min(lista))
 
+# Imprime números pares
 print('Números pares: ', end='')
 for numero in lista:
     if numero % 2 == 0:
         print('{}; '.format(numero), end='')
 
+# Imprime o número de ocorrências do 1º elemento da lista
 elemento_1 = lista[0]
 print('\nOcorrências do 1º elemento: ', lista.count(elemento_1))
