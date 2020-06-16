861739daa531be57e25a28a084d67dadb6715a15
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-06-16 15:15:10 -0300

    Exercício 4.5.4 - Impressão dos dados na tela

--- capitulo_4/cadastro_de_usuarios.py
+++ capitulo_4/cadastro_de_usuarios.py
@@ -3,8 +3,11 @@
 # Autor: Pinheiro Jr.
 # Data: 16/06/2020
 
-dict = {}
+dict_usuario = {}
 
-dict['nome'] = input('Informe o nome: ')
-dict['idade'] = input('Informe a idade: ')
-dict['cidade'] = input('Informe a cidade: ')
+dict_usuario['nome'] = input('Informe o nome: ')
+dict_usuario['idade'] = input('Informe a idade: ')
+dict_usuario['cidade'] = input('Informe a cidade: ')
+
+for key in dict_usuario.keys():
+    print(key + ': ' + dict_usuario[key])
