8cee4b0ab6b6063c6306324f650dd278c8b9966a
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-18 22:42:35 -0300

    Exercício 5.5.4 - Teste de função

--- capitulo_5/funcoes.py
+++ capitulo_5/funcoes.py
@@ -1,5 +1,5 @@
 # Arquivo: funcoes.py (UTF-8)
-# Descrição: Função para calcular velocidade média. Exercício 5.5.3
+# Descrição: Função para calcular velocidade média. Exercício 5.5.4
 # Autor: Pinheiro Jr.
 # Data: 18/07/2020
 
@@ -9,3 +9,8 @@
 def velocidade_media(distancia, tempo):
     velocidade = distancia/tempo
     print(velocidade)
+
+velocidade_media(100,20)
+velocidade_media(150,22)
+velocidade_media(200,20)
+velocidade_media(50,3)
