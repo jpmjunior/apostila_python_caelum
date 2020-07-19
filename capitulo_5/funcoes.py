7fb466f02aa9f3c75129c8448401c4fc0c4f7431
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-18 23:15:44 -0300

    Exercício 5.5.11 - Função divisao() definida

--- capitulo_5/funcoes.py
+++ capitulo_5/funcoes.py
@@ -1,5 +1,5 @@
 # Arquivo: funcoes.py (UTF-8)
-# Descrição: Prática de funções. Exercício 5.5.10
+# Descrição: Prática de funções. Exercício 5.5.11
 # Autor: Pinheiro Jr.
 # Data: 18/07/2020
 
@@ -7,7 +7,7 @@
 #   distancia: metros
 #   tempo: segundos
 def velocidade_media(distancia, tempo):
-    return distancia/tempo
+    return divisao(distancia,tempo)
 
 def soma(num1, num2):
     return num1+num2
@@ -15,9 +15,12 @@ def soma(num1, num2):
 def subtracao(num1, num2):
     return num1-num2
 
+def divisao(num1,num2):
+    return num1/num2
+
 def calculadora(num1, num2):
     return soma(num1,num2), subtracao(num1,num2), num1*num2, num1/num2
 
-print(calculadora(10,3))
-print(calculadora(2,6))
-print(calculadora(3,4.2))
+print(velocidade_media(100,20))
+print(velocidade_media(-20,10))
+print(velocidade_media(150,0))
