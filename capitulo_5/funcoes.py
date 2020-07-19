33c282c6717a62b5737a731919b99b1cd5fda040
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-18 22:31:03 -0300

    Exercício 5.5.1 - Função velocidade_media() definida

--- capitulo_5/funcoes.py
+++ capitulo_5/funcoes.py
@@ -2,3 +2,9 @@
 # Descrição: Função para calcular velocidade média. Exercício 5.5.1
 # Autor: Pinheiro Jr.
 # Data: 18/07/2020
+
+# Função: velocidade_media()
+#   distancia: metros
+#   tempo: segundos
+def velocidade_media(distancia, tempo):
+    pass
