3aaf80b7fb49394937c19fdbdd59d8cf0f439ffd
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-23 18:18:41 -0300

    Exercício 5.8.2 - Exibindo uma tupla de argumentos

--- capitulo_5/teste_args_kwargs.py
+++ capitulo_5/teste_args_kwargs.py
@@ -1,5 +1,5 @@
 # Arquivo: teste_args_kwargs.py (UTF-8)
-# Descrição: Funções com número variável de parâmetros. Exercício 5.8.1
+# Descrição: Funções com número variável de parâmetros. Exercício 5.8.2
 # Autor: Pinheiro Jr.
 # Data: 23/07/2020
 
@@ -7,3 +7,6 @@ def teste_args_kwargs (arg1, arg2, arg3):
     print('arg1: ', arg1)
     print('arg2: ', arg2)
     print('arg3: ', arg3)
+
+args = ('um', 2, 3)
+teste_args_kwargs(*args)
