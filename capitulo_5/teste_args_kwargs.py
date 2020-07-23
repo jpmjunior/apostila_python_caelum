74e044d381b6ab7931dd24adf5f0cd23313a3daf
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-23 18:15:44 -0300

    Exercício 5.8.1 - Exibir argumentos da função

--- capitulo_5/teste_args_kwargs.py
+++ capitulo_5/teste_args_kwargs.py
@@ -2,3 +2,8 @@
 # Descrição: Funções com número variável de parâmetros. Exercício 5.8.1
 # Autor: Pinheiro Jr.
 # Data: 23/07/2020
+
+def teste_args_kwargs (arg1, arg2, arg3):
+    print('arg1: ', arg1)
+    print('arg2: ', arg2)
+    print('arg3: ', arg3)
