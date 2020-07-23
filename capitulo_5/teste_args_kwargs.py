e3466e8c4b2f68c9a8ec16ad28174fe76cecfee6
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-23 18:27:03 -0300

    Exercício 5.8.3 - Exibindo um dicionário recebido como arumentos

--- capitulo_5/teste_args_kwargs.py
+++ capitulo_5/teste_args_kwargs.py
@@ -1,5 +1,5 @@
 # Arquivo: teste_args_kwargs.py (UTF-8)
-# Descrição: Funções com número variável de parâmetros. Exercício 5.8.2
+# Descrição: Funções com número variável de parâmetros. Exercício 5.8.3
 # Autor: Pinheiro Jr.
 # Data: 23/07/2020
 
@@ -8,5 +8,5 @@ def teste_args_kwargs (arg1, arg2, arg3):
     print('arg2: ', arg2)
     print('arg3: ', arg3)
 
-args = ('um', 2, 3)
-teste_args_kwargs(*args)
+kwargs = {'arg1': 'um', 'arg3': 3, 'arg2': 'dois'}
+teste_args_kwargs(**kwargs)
