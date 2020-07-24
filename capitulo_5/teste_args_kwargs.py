d954c4ad7e6d6450f2d16be5189652cce38e4c90
Pinheiro Jr <junior.linhares@yahoo.com.br>
2020-07-23 21:44:57 -0300

    Exercício 5.8.5 - Solução para 4 argumentos

--- capitulo_5/teste_args_kwargs.py
+++ capitulo_5/teste_args_kwargs.py
@@ -1,15 +1,14 @@
 # Arquivo: teste_args_kwargs.py (UTF-8)
-# Descrição: Funções com número variável de parâmetros. Exercício 5.8.4
+# Descrição: Funções com número variável de parâmetros. Exercício 5.8.5
 # Autor: Pinheiro Jr.
 # Data: 23/07/2020
 
-def teste_args_kwargs (arg1, arg2, arg3):
+def teste_args_kwargs (arg1, arg2, arg3, arg4):
     print('arg1: ', arg1)
     print('arg2: ', arg2)
     print('arg3: ', arg3)
+    print('arg4: ', arg4)
 
 kwargs = {'arg1': 'um', 'arg3': 3, 'arg2': 'dois', 'arg4': 4}
 
-# Ocorre erro de compilação, pois a função está definida para
-# 3 argumentos e o dicionário passado possui 4
 teste_args_kwargs(**kwargs)
